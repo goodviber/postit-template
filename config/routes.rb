bdde1e77e492c656189fb2450601debbf29a6070
Ed Mills <molarexplorer@yahoo.co.uk>
2013-09-20 23:45:46 +0100

    url added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 PostitTemplate::Application.routes.draw do
   root to: 'posts#index'
-  resources :posts, :except => :destroy do
+  resources :posts do
   	resources :comments, :only => :create
   end
 end
