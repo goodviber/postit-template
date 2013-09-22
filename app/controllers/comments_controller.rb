99b4bdc213441cdf6d553360617a4ef5e3a9ddd7
Ed Mills <molarexplorer@yahoo.co.uk>
2013-09-23 00:18:39 +0100

    comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -5,13 +5,24 @@ class CommentsController < ApplicationController
 	end
 
 	def create
-		@comment=Comment.new(post_params)
-		if comment.save
+		@post = Post.find(params[:post_id])
+		@comment = Comment.new(comment_params)
+		@comment.post = @post
+
+		@comment.user_id = 1
+
+		if @comment.save
 			flash[:notice] = "You have created a comment"
-			redirect_to_post_comments_path
+			redirect_to post_path(@post)
 		else
-			render 'new'
+			render 'posts/show'
 		end
 
+	end
+
+	private
+
+	def comment_params
+		params.require(:comment).permit(:body)
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 PostitTemplate::Application.routes.draw do
   root to: 'posts#index'
+  
   resources :posts do
   	resources :comments, :only => :create
   end
