9c4459f7c3e0132b689d365de491edc7432da26d
Ed Mills <molarexplorer@yahoo.co.uk>
2013-09-23 14:41:08 +0100

    ?

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ActiveRecord::Base
 	belongs_to :user, foreign_key: :user_id
-	has_many :comments, dependent: :destroy
 	has_many :categories, through: :post_categories
+	has_many :comments, dependent: :destroy
 	validates :title, :url, :description, presence: true, length: {minimum: 5}
 end
