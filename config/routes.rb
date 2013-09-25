7342d3c74202d2e26d0b529dbb8f7d4ebbcb7e40
Ed Mills <molarexplorer@yahoo.co.uk>
2013-09-25 22:18:21 +0100

    no nav display

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 PostitTemplate::Application.routes.draw do
   root to: 'posts#index'
+
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  get '/logout', to: 'sessions#destroy'
   
   resources :posts do
   	resources :comments, :only => :create
   end
+  resources :users
     resources :categories, only: [:new, :create, :show]
 end
