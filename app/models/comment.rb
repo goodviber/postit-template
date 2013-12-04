f18a9f49355fe17947f6bff0e35446b31aa2e9f0
Ed Mills <molarexplorer@yahoo.co.uk>
2013-12-04 18:58:14 +0000

    js comments vots

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,12 +1,14 @@
 class Comment <ActiveRecord::Base
+	include Voteable
+	
 	belongs_to :user, foreign_key: :user_id
 	belongs_to :post, foreign_key: :post_id
-	has_many :votes, as: :voteable
+	#has_many :votes, as: :voteable
 
 
 	validates :body, presence: true
 
-def total_votes
-			self.votes.where(vote: true).size - self.votes.where(vote: false).size
-	end
+#def total_votes
+#			self.votes.where(vote: true).size - self.votes.where(vote: false).size
+#	end
 end
